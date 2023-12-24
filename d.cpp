1177cdd261fb4fa047a9ea94b22ac7fc5c9bbfd6
masoumehSaberi <129509775+masoumehSaberi@users.noreply.github.com>
2023-12-24 19:52:01 +0300

    d.cpp

---
+++ d.cpp
@@ -0,0 +1,60 @@
+#include <climits>
+#include <iostream>
+#include <queue>
+#include <vector>
+
+void AddEdge(std::vector<std::vector<int>>& graph, int u, int v) {
+  graph[u].push_back(v);
+  graph[v].push_back(u);
+}
+
+void BFS(std::vector<std::vector<int>>& graph, int n, int source,
+         std::vector<int>& distance) {
+  std::queue<int> q;
+  q.push(source);
+  std::vector<bool> visited(n, false);
+  visited[source] = true;
+  distance[source] = 0;
+  while (!q.empty()) {
+    int k = q.front();
+    q.pop();
+    for (auto i : graph[k]) {
+      if (!visited[i]) {
+        q.push(i);
+        distance[i] = distance[k] + 1;
+        visited[i] = true;
+      }
+    }
+  }
+}
+
+int PathLength(std::vector<std::vector<int>>& graph, int n, int src1, int src2,
+               int dest) {
+  constexpr int kMaxInt = std::numeric_limits<int>::max();
+  std::vector<int> leon_dist(n, kMaxInt), matilda_dist(n, kMaxInt),
+      milk_dist(n, kMaxInt);
+  BFS(graph, n, src1, leon_dist);
+  BFS(graph, n, src2, matilda_dist);
+  BFS(graph, n, dest, milk_dist);
+  int answer = std::numeric_limits<int>::max();
+  for (int i = 0; i < n; i++) {
+    if (leon_dist[i] + matilda_dist[i] + milk_dist[i] < answer) {
+      answer = leon_dist[i] + matilda_dist[i] + milk_dist[i];
+    }
+  }
+  return answer;
+}
+
+int main() {
+  int n, m, src1, src2, dest, v1, v2;
+  std::cin >> n;
+  std::cin >> m;
+  std::cin >> src1 >> src2 >> dest;
+  std::vector<std::vector<int>> graph(n + 1);
+  for (int i = 0; i < m; i++) {
+    std::cin >> v1 >> v2;
+    AddEdge(graph, v1 - 1, v2 - 1);
+  }
+  std::cout << PathLength(graph, n, src1 - 1, src2 - 1, dest - 1) << std::endl;
+  return 0;
+}
